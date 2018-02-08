7ad42d0eacafce58543d22da993157549b013ccd
fuzzzyy17 <faizan_7-8-6@hotmail.co.uk>
2018-02-08 04:40:47 +0000

    9_1 8/2

--- rango/forms.py
+++ rango/forms.py
@@ -1,5 +1,7 @@
 from django import forms
-from rango.models import Page, Category
+from django.contrib.auth.models import User
+from rango.models import Category, Page, UserProfile
+
 
 class CategoryForm(forms.ModelForm):
     name = forms.CharField(max_length = 128,
@@ -11,6 +13,7 @@ class CategoryForm(forms.ModelForm):
     class Meta:
         model = Category
         fields = ('name',)
+
 
 class PageForm(forms.ModelForm):
     title = forms.CharField(max_length = 128,
@@ -33,4 +36,17 @@ class PageForm(forms.ModelForm):
         model = Page
         exclude = ('category',)
 
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget = forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+
+class UserProfileForm(forms.ModelForm):
     
+    class Meta:
+        model = UserProfile
+        fields = ('website', 'picture')
